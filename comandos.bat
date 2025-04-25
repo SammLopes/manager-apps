a039da77c2a8464958927bbf0ce9dc47e9549985
SammLopes <sam.v.p.lopes@gmail.com>
2025-04-25 08:42:16 -0300

    Adicionado arquivos comandos.bat para um CLI pequeno para automatizar poucas coisa, isso ainda vai dar muito erro espero que funcione XD

---
+++ comandos.bat
@@ -0,0 +1,65 @@
+@echo off
+echo Qual programa vai abrir hj Samuel?
+echo WebStorm - 1
+echo PhpStorm - 2
+echo Android Studio - 3
+echo PyCharm - 4
+echo Iniciar VM -5
+echo Encerrar VM -6
+echo Sair - 0
+
+set /p soft=Digite um Numero: 
+
+if "%soft%"=="1" (
+  echo Abrindo WebStorm...
+  start "" "C:\Program Files\JetBrains\WebStorm 2025.1\bin\webstorm64.exe"
+  GOTO :fim
+)
+
+if "%soft%"=="2" (
+  echo Abrindo PhpStorm...
+  start "" "C:\Program Files\JetBrains\PhpStorm 2024.3.4\bin\phpstorm64.exe"
+  GOTO :fim
+)
+
+if "%soft%"=="3" (
+  echo Abrindo Android Studio...
+  start "" "C:\Program Files\Android\Android Studio\bin\studio64.exe"
+  GOTO :fim
+)
+
+if "%soft%"=="4" (
+  echo Abrindo PyCharm...
+  start "" "C:\Program Files\JetBrains\PyCharm 2024.3.5\bin\pycharm64.exe"
+  GOTO :fim
+)
+
+if "%soft%"=="5" (
+  echo Iniciando VM........ 
+  start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe"
+  CALL :changeDicVm
+  vagrant reload --provision
+  GOTO :fim
+)
+
+if "%soft%"=="6" (
+  echo Encerrando VM.....
+  CALL :changeDicVm
+  vagrant halt
+  GOTO :fim
+)
+
+if "%soft%"=="0" (
+  GOTO :fim
+)
+
+echo Opcao invalida
+pause
+GOTO :fim
+
+:changeDicVm
+cd /d "C:\Homestead\"
+GOTO :EOF
+
+:fim 
+echo fim do programa
