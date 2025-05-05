25fd36dc6883fc27b21c37bbd535592dad6c7223
SammLopes <sam.v.p.lopes@gmail.com>
2025-05-05 14:13:35 -0300

    Adicionado logica de vms dendo do vm.bat

--- comandos/vm/vm.bat
+++ comandos/vm/vm.bat
@@ -1,14 +1,48 @@
 @echo off
 echo arquivo de manipulação de VM do Virtual Box
 echo nesse arquivo começamos a analisar o segundo parametro informado, sem contar o primeiro nome.
+echo %2 segundo parametro
+ 
+if "%2" == "init" (
+  echo Iniciando VM........ 
+  start "" "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe"
+  CALL :changeDicVm
+  vagrant reload --provision
+  GOTO :fim
+)
 
-//Se o parametro for init - Iniciar VM 
+if "%2" == "stop" (
+  echo Encerrando VM.....
+  CALL changeDicVm
+  vagrant halt
+  GOTO :fim
+)
 
-//Se o parametro for stop - Encerrar VM
+if "%2"=="enter" (
+  echo Acessando ssh VM......
+  CALL :changeDicVm
+  vagrant ssh
+  GOTO :fim
+)
 
-//Se o parametro for enter - Acessar VM Via ssh
+if "%2" == "help" (
+  CALL :help
+  GOTO :fim
+)
 
+:help 
+echo ========================================
+echo       MENU DE PROGRAMAS DO SAMUEL
+echo ========================================
+echo [init] Iniciar VM 
+echo [stop] Para VM
+echo [enter] Acessar VM via SSH 
+echo ========================================
+GOTO :EOF
 
 :changeDicVm
 cd /d "C:\Homestead\"
 GOTO :EOF
+
+:fim 
+echo fim do programa
