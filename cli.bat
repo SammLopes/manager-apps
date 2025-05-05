df9ae1e6402c3cff6fb986d7a8b3577cdce91b6a
SammLopes <sam.v.p.lopes@gmail.com>
2025-05-05 11:36:26 -0300

    Update cli.bat ainda sem testar

--- cli.bat
+++ cli.bat
@@ -2,16 +2,46 @@
 echo Arquivo cli.bat 
 
 //Redirecionar para phpstorm.bat
-
+if "%1" == "phpstorm" (
+  CALL comandos\phpstorm.bat 
+  GOTO :fim
+)
 //Redirecionar para websotorm.bat
-
+if "%1" == "webstorm"(
+  CALL comandos\webstorm.bat
+  GOTO :fim
+)
 //Redirecionar para pycharm.bat
-
-//Redicerionar para websotorm.bat 
-
-//Redirecionar para websotorm.php
-
+if "%1" == "pycharm" (
+  CALL comandos\pycharm.bat  
+  GOTO :fim
+)
+//Redicerionar para android-studio.bat 
+if "%1" == "andoroid" (
+  CALL comandos\android.bat
+  GOTO :fim
+)
 //Redirecionar para vm.bat
-
+if "%1" == "vm" (
+  CALL comandos\vm\vm.bat
+  GOTO :fim
+)
 //Caso não seja nenhum deles mostramos a tela de help
+if "%1" == "help" (
+  GOTO :help 
+  GOTO :fim
+)
 
+:help 
+echo ========================================
+echo       MENU DE PROGRAMAS DO SAMUEL
+echo ========================================
+echo [webstorm] Abrir WebStorm
+echo [phpstorm] Abrir PhpStorm
+echo [android] Android Studio 
+echo [pycharm] PyCharm 
+echo ========================================
+GOTO :EOF
+
+:fim 
+echo fim do programa
