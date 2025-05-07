0eae1174617f09b822a172fa6d85687bad533c49
SammLopes <sam.v.p.lopes@gmail.com>
2025-05-07 13:50:20 -0300

    Adicionado dic do git no projeto e alterado o timeout para executção do phpstorm

---
+++ comandos/git/git.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Arquivo de facildiade do git
+
+:: Integraçaõ com os comandos de configurações :
+:: git config --list 
+:: git config --global user.name <nome>
+:: git config --global user.email <email>
+:: Adicionar reporitórios 
+:: git remote add origin <url>
+:: Lista repostórios 
+:: git remote -v
+
